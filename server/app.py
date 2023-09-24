872193de16c49c1e9d3fa8e411e013e865b99a72
Justin Bigishiro <justin.rvd@gmail.com>
2023-09-24 02:21:17 -0500

    finish lab

--- server/app.py
+++ server/app.py
@@ -18,6 +18,20 @@ db.init_app(app)
 
 api = Api(app)
 
+@app.before_request
+def is_member_only():
+
+    if not session.get('user_id') \
+        and request.endpoint != "login":
+        return {'error': "Unauthorizd"}, 401
+    
+    elif request.endpoint != "member_index" \
+        and request.endpoint != "member_article"\
+        and request.endpoint != "login" \
+        and request.endpoint != "logout":
+        
+        return {'error': "Unauthorizd"}, 401
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -87,12 +101,16 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        articles = [article.to_dict() for article in Article.query.all() if article.is_member_only == True]
+
+        return make_response((articles), 200)
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        article = Article.query.filter(Article.id == id).first()
+        article_json = article.to_dict()
+        return article_json, 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
